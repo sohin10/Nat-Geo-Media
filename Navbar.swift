a27716434fcffd12f2fb4a7cd0e4284d1119359f
sohin10 <60220627+sohin10@users.noreply.github.com>
2022-12-11 23:00:12 -0700

    Add files via upload

---
+++ Navbar.swift
@@ -0,0 +1,20 @@
+//
+//  Navbar.swift
+//  NatGeo App
+//
+//  Created by Sohini Pandit on 01/12/22.
+//
+
+import SwiftUI
+
+struct Navbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Navbar_Previews: PreviewProvider {
+    static var previews: some View {
+        Navbar()
+    }
+}
