f29d8f5a98316da1590803eab2d75aa4aa7211c3
sohin10 <60220627+sohin10@users.noreply.github.com>
2022-12-11 23:13:03 -0700

    Add files via upload

---
+++ Swift Files/NatGeo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatGeo_AppApp.swift
+//  NatGeo App
+//
+//  Created by Sohini Pandit on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NatGeo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreen()
+        }
+    }
+}
